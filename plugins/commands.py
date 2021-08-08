0db57e50a28404e2f96dab7ed362f439312ddb17
ak3052005 <37368724+ak3052005@users.noreply.github.com>
2021-08-08 17:54:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,12 +75,12 @@ You can also use /dplay <song name> to play a song from Deezer.</b>
 async def start(client, message):
     buttons = [
         [
-        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-        InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
+        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/Movies_Unloaded'),
+        InlineKeyboardButton('🤖 Other Bots', url='https://t.me/Movies_Unloaded'),
     ],
     [
-        InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-        InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/pro_Morningstar'),
+        InlineKeyboardButton('🧩 Source', url='https://t.me/pro_morningstar'),
     ],
     [
         InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help'),
@@ -98,12 +98,12 @@ async def start(client, message):
 async def show_help(client, message):
     buttons = [
         [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
+            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/Movies_Unloaded'),
+            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/Movies_Unloaded'),
         ],
         [
-            InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+            InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/pro_morningstar'),
+            InlineKeyboardButton('🧩 Source', url='https://t.me/pro_morningstar'),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
